0aa575c01fe5a5122e5413b82e9ae25a7d38aac9
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-30 20:46:06 -0700

    created variable for if else statements

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,47 @@
 var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
+//utilized moment.js to be able to display the current time 
+var time = moment().format("hh:mm:ss");
+$("#timeSlot").text(time);
+
+//get live time above???
+
+
+//functions below
+var hour9 = $("#hr9"); 
+var hour10 = $("#hr10"); 
+var hour11 = $("#hr11"); 
+var hour12 = $("#hr12"); 
+var hour1 = $("#hr1");
+var hour2 = $("#hr2"); 
+var hour3 = $("#hr3"); 
+var hour4 = $("#hr4"); 
+var hour5 = $("#hr5"); 
+var hour6 = $("#hr6");
+
+var hour = moment().hour();
+console.log(hour);
+
+function currentTime (bfr, afr) {
+    if (hour == bfr) {
+        afr.addClass("present");
+    }
+    else if(hour < bfr) {
+        afr.addClass("future");
+    }
+    else if(hour > bfr) {
+        afr.addClass("past");
+    }
+}
+currentTime(9, hour9);
+currentTime(10, hour10);
+currentTime(11, hour11);
+currentTime(12, hour12);
+currentTime(1, hour1);
+currentTime(2, hour2);
+currentTime(3, hour3);
+currentTime(4, hour4);
+currentTime(5, hour5);
+currentTime(6, hour6);
 
